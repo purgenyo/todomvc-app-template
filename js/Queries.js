2f85e6384bf51bb62e0acf9fea77a361373dfdea
nenashev <nenashev@amlsys.ru>
2017-09-12 17:43:09 +0700

    Функция запроса

--- js/Queries.js
+++ js/Queries.js
@@ -1,10 +1,15 @@
 class Queries {
 	runQuery(method, data, params){
-		return $.ajax({
-			dataType: "json",
-			method: method,
-			data: JSON.stringify(data),
-			url: config.api_server + '/todo/',
-		});
+
+		let request_params = params;
+		request_params.dataType = 'json';
+		request_params.method = method;
+		request_params.url =  config.api_server + '/todo/';
+
+		if(data!==null){
+			request_params.data =  data;
+		}
+
+		return $.ajax(request_params);
 	}
 }
