9d8c40d8cf5c0629c9d3e5d3b5351df1b4a0ff11
nenashev <nenashev@amlsys.ru>
2017-09-13 09:06:07 +0700

    update логика (todo)

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 	let queries = new Queries();
 	let todoList = new TodoList();
 
-	var api_url = config.todo_api_server + '/todo';
+	let api_url = config.todo_api_server + '/todo';
 	queries.runQuery('GET', api_url, null, {})
 		.done((res)=>todoList.renderElement(res))
 		.fail((xhr, text)=>{ console.log(xhr, text) });
@@ -16,31 +16,71 @@
 
 
 	$(document).on('dblclick', '.todo-list li .view label', (e)=>{
-		var element_root = $(e.target).parents('li');
-		var edit = element_root.children('.edit');
-		var view = element_root.children('.view');
+		let element_root = $(e.target).parents('li');
+		let edit = element_root.children('.edit');
+		let view = element_root.children('.view');
 		edit.show();
 		edit.focus();
 		view.hide();
 
 	});
 
-	$(document).on('blur', '.edit', (e)=>{
-		var element_root = $(e.target).parents('li');
-		var edit = element_root.children('.edit');
-		var view = element_root.children('.view');
-		edit.hide();
-		view.show();
+	$(document).on('keyup', '.edit', (e)=>{
+		if(e.keyCode===13){
+			todoProcessEdit(e);
+		}
 	});
 
-	$(document).on('click', '.todo-list li .view input', (e)=>{
-		console.log('input');
+	$(document).on('blur', '.edit', (e)=>{
+		todoProcessEdit(e);
 	});
+
+	function todoProcessEdit(e) {
+		let element_root = $(e.target).parents('li');
+		let todo_id = element_root.data('todoid');
+		let edit = element_root.children('.edit');
+		let view = element_root.children('.view');
+		edit.hide();
+		view.show();
+		let data = {text: edit.val()}
+		todoChanged(todo_id, data);
+	}
+
+	$(document).on('change', '.todo-list li .view input', (e)=>{
+		let root_element = $(e.target).parents('li');
+		let todo_id = root_element.data('todoid');
+
+		let status_new = 1;
+		if($(e.target).is(':checked')){
+			status_new = 2;
+		}
+		let data = {status: status_new}
+		todoChanged(todo_id, data);
+	});
+
+	function todoChanged(todo_id, data){
+		let api_url = config.todo_api_server + '/todo/' + todo_id;
+		queries.runQuery('PUT', api_url, data, {})
+			.done((res)=>{
+				let li = $(`li[data-todoid='${todo_id}']`);
+				let edit = li.children('.edit');
+				let label = li.children('.view').children('label');
+				edit.val(res.data.text);
+				label.text(res.data.text);
+				if(res.data.status===1){
+					li.removeClass('completed');
+				} else {
+					li.addClass('completed');
+				}
+
+			})
+			.fail((xhr, text)=>{ console.log(xhr, text) });
+	}
 
 	$(document).on('click', '.todo-list li .view .destroy', (e)=>{
 		let root_element = $(e.target).parents('li');
 		let todo_id = root_element.data('todoid');
-		var api_url = config.todo_api_server + '/todo/' + todo_id;
+		let api_url = config.todo_api_server + '/todo/' + todo_id;
 		$(e.target).attr('disabled', true);
 		queries.runQuery('DELETE', api_url, null, {})
 			.done((res)=>{
@@ -54,16 +94,20 @@
 
 	$(document).on('keyup', '.new-todo', (el)=>{
 		if(el.keyCode===13){
-			var api_url = config.todo_api_server + '/todo/';
+			$(el.target).attr('disabled', true);
+			let api_url = config.todo_api_server + '/todo/';
 			queries.runQuery('POST', api_url, {
 				'text': el.target.value
 			}, {})
 			.done((res)=>{
 				el.target.value = '';
+				$(el.target).attr('disabled', false);
 				todoList.renderElement( {data: [res.data]} )
 			})
-			.fail((xhr, text)=>{ console.log(xhr, text) });
+			.fail((xhr, text)=>{
+				$(el.target).attr('disabled', false);
+				console.log(xhr, text)
+			});
 		}
 	});
-
 })(window);
